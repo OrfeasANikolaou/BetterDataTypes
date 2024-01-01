f55d7c81696560c969f8b038f16a74221b394e73
OrfeasANikolaou <orfeasnikolaou.dev@gmail.com>
2024-01-01 18:28:20 +0200

    made repo added basic header and LICENSE

---
+++ better_datatypes.h
@@ -0,0 +1,18 @@
+#ifndef ORFEAS_NIKOLAOU_RANDOM_HEADERS_BETTER_DATATYPES_H_
+#define ORFEAS_NIKOLAOU_RANDOM_HEADERS_BETTER_DATATYPES_H_
+#include <stdint.h>
+#include <uchar.h>
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+typedef int8_t i8; // signed char NEVER USE THIS
+typedef uint8_t u8; // unsigned char
+typedef float f32;
+typedef double f64;
+typedef long double f128;
+typedef char16_t uch16; // wide chars
+typedef char32_t uch32; // wide chars
+#endif // ORFEAS_NIKOLAOU_RANDOM_HEADERS_BETTER_DATATYPES_H_
