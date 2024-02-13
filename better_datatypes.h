fe5df26579f10c9f7a7c8015f6385417cde129f2
OrfeasANikolaou <orfeasnikolaou.dev@gmail.com>
2024-02-13 19:38:48 +0200

    just use char it's more intuitive

--- better_datatypes.h
+++ better_datatypes.h
@@ -8,8 +8,6 @@ typedef int64_t i64;
 typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
-typedef int8_t i8; // signed char NEVER USE THIS
-typedef uint8_t u8; // unsigned char
 typedef float f32;
 typedef double f64;
 typedef long double f128;
